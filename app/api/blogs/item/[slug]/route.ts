23f9b31774d4758dc3864f4c2deec1a3b6203d83
wissem-thinklab <wissemchihaoui@thinklab.tn>
2025-12-18 09:51:34 +0100

    fix: validate empty related blogs

--- app/api/blogs/item/[slug]/route.ts
+++ app/api/blogs/item/[slug]/route.ts
@@ -65,7 +65,7 @@ export async function GET(
         prev: prevBlog || null,
         next: nextBlog || null
       },
-      related: relatedBlogs
+      related: relatedBlogs || []
     });
 
   } catch (error) {
